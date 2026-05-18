56e53053e5cb96d580555b5e10ea2e307ca5c99c
ajwanbe <ajwanbebar@gmail.com>
2026-05-18 13:43:18 +0300

    Update server.py

--- server.py
+++ server.py
@@ -31,20 +31,5 @@ def search():
 
     return jsonify(results)
 
-@app.route("/audio")
-def audio():
-    track_id = request.args.get("id")
-
-    url = f"https://api.jamendo.com/v3.0/tracks/?client_id={API_KEY}&format=json&id={track_id}"
-
-    r = requests.get(url)
-    data = r.json()
-
-    if data["results"]:
-        return data["results"][0]["audio"]
-
-    return "not found", 404
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
